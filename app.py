e1a0c03f11d4d08fb29c1c6a4db41c6c2e0e8e2c
Diana BUYINZA <buyinzadiana@gmail.com>
2026-02-19 15:59:42 +0200

    print

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ print("hello world")
 
 if 5 > 2:
     print("Five is greater than two!")
-    print ("python is fun" \
-    "")
+    print("python is fun"
+          "")
+
+
+# EXERCISE
+
+
+print("Hello World"), print("Have  a good day"), print("Learning Python Is Fun")
