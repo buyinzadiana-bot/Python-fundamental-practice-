0bdfc2e70182ddf169bdf4ee87c72f48a7440da9
Diana BUYINZA <buyinzadiana@gmail.com>
2026-02-19 15:48:52 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+
+"""Module description here: explains what this module does."""
+
+
+print("hello world")
+
+if 5 > 2:
+    print("Five is greater than two!")
+    print ("python is fun" \
+    "")
