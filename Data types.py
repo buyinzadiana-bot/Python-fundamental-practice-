8e6466e5b1e5947127424fda20328d8a1085909b
Diana BUYINZA <buyinzadiana@gmail.com>
2026-02-20 11:21:47 +0200

    data types

---
+++ Data types.py
@@ -0,0 +1,7 @@
+# integer (int) = whole numbers  (10, 20, 30, 40)
+# Floats = decimal numbers   (10.5, 20.5, 30.5)
+# String (str) = text  ("Hello", "Python", "Diana")
+# Boolean (bool) = True or False
+# List = a collection of items (fruits = ["apple", "banana", "orange"])
+# Tuple = a collection of items that cannot be changed (fruits = ("apple", "banana", "orange"))
+# Dictionary = a collection of key-value pairs (person = {"name":
