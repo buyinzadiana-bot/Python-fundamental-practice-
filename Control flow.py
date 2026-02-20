b216c892ff66b0b0ca67747a470035c90e461c0a
Diana BUYINZA <buyinzadiana@gmail.com>
2026-02-20 11:45:23 +0200

    elif

--- Control flow.py
+++ Control flow.py
@@ -1,9 +1,22 @@
-# if else 
+# if else
 
 age = 18
 
 if age >= 18:
     print("you are an adult")
 
-else: 
-    print ("you are a minor")
+else:
+    print("you are a minor")
+
+    # elif
+
+    score = 85
+
+    if score >= 80:
+        print("Grade: A")
+
+    elif score >= 70:
+        print("Grade: B")
+
+    else:
+        print("Grade: C")
