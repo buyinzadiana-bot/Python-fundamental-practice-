b18dfcd05b4e4a623df24b9b99af3e76c4553679
Diana BUYINZA <buyinzadiana@gmail.com>
2026-02-20 11:33:29 +0200

    operators

---
+++ operators.py
@@ -0,0 +1,25 @@
+# Arithmetic (math)
+# +, -, *, /, %, ** (exponentiation), // (floor division)
+
+a = 10
+b = 5
+
+print (a + b) # 15
+print (a - b) # 5
+print (a * b) # 50
+print (a / b) # 2.0
+print (a % b) # 0
+print (a ** b) # 100000
+print (a // b) # 2
+
+# comaparison
+print (a > b) # True
+print (a < b) # False
+print (a == b) # False
+print (a != b) # True
+
+# logical combining question
+
+print (a > 5 and b < 10) # True
+print (a > 5 or b > 10) # True  
+print (not (a > 5)) # False 
